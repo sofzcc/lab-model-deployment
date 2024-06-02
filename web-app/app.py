ee632266650e6f4ced5a59dc20f209719ff5b61f
sofzcc <100369299+sofzcc@users.noreply.github.com>
2024-06-02 17:45:21 +0200

    final submission

--- web-app/app.py
+++ web-app/app.py
@@ -17,17 +17,17 @@ def predict():
 
     int_features = [int(x) for x in request.form.values()]
     final_features = [np.array(int_features)]
+    
     prediction = model.predict(final_features)
-
     output = prediction[0]
 
     #countries = ["Australia", "Canada", "Germany", "UK", "US"]
-    shape = ['cylinder', 'light',  'circle',  'sphere',  'disk',  'fireball',  'unknown',  'oval', 
- 'other',  'cigar',  'rectangle',  'chevron',  'triangle',  'formation', 'delta', 
- 'changing',  'egg',  'diamond',  'flash',  'teardrop',  'cone',  'cross',  'pyramid', 
- 'round',  'crescent',  'flare',  'hexagon',  'dome',  'changed']
+    shapes = ['Cylinder', 'Light', 'Circle', 'Sphere', 'Disk', 'Fireball', 'Unknown', 'Oval',
+          'Other', 'Cigar', 'Rectangle', 'Chevron', 'Triangle', 'Formation', 'Delta',
+          'Changing', 'Egg', 'Diamond', 'Flash', 'Teardrop', 'Cone', 'Cross', 'Pyramid',
+          'Round', 'Crescent', 'Flare', 'Hexagon', 'Dome', 'Changed']
     return render_template(
-        "index.html", prediction_text="The most likely shape is: {}".format(shape[output])
+        "index.html", prediction_text="The most likely shape is: {}".format(shapes[output])
     )
 
 
